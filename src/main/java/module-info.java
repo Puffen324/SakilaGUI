4cbcf931cd29217896aad17b9e4c6ded72a971e9
André Söderlund <andre.soderlund@live.com>
2022-03-30 11:12:53 +0200

    Skapat lite olika paket

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module com.yaelev.sakilagui {
 
     opens com.yaelev.sakilagui to javafx.fxml;
     exports com.yaelev.sakilagui;
+    exports com.yaelev.sakilagui.controllers;
+    opens com.yaelev.sakilagui.controllers to javafx.fxml;
+
 }
