3296c502f8284b6feb0b458b8acf6b843959463e
André Söderlund <andre.soderlund@live.com>
2022-03-31 15:35:27 +0200

    Skapat abstrakt DAO klass samt implementerat CRUD för ActorDAO

---
+++ src/main/java/com/yaelev/sakilagui/dao/GenericDAO.java
@@ -0,0 +1,36 @@
+package com.yaelev.sakilagui.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public abstract class GenericDAO<E> {
+
+    public static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("hibernate");
+
+    protected EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+    protected EntityTransaction transaction = null;
+
+    public void executeInsideTransaction(Consumer<EntityManager> action) {
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            action.accept(entityManager);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally{
+            entityManager.close();
+    }
+}
+
+}
