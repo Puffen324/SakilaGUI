79b4b06523ac2ece4ef887c9ab1f0162faeadf02
Joakim Gårdhagen <superapa.spanx@gmail.com>
2022-03-31 09:32:34 +0200

    test

--- src/main/java/com/yaelev/sakilagui/Main.java
+++ src/main/java/com/yaelev/sakilagui/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-// timmar vi lagt såhär långt är ca 5Timmar * 3 
+// timmar vi lagt såhär långt är ca 5Timmar * 3
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
---
+++ src/main/java/com/yaelev/sakilagui/dao/DataEntityManger.java
@@ -0,0 +1,4 @@
+package com.yaelev.sakilagui.dao;
+
+public class DataEntityManger {
+}
