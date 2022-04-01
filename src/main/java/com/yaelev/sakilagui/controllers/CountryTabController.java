6616cc6b6fc58a925c0fbba0243bc47182b50ed1
André Söderlund <andre.soderlund@live.com>
2022-04-01 13:47:48 +0200

    Ordnat countrytab backend

--- src/main/java/com/yaelev/sakilagui/controllers/CountryTabController.java
+++ src/main/java/com/yaelev/sakilagui/controllers/CountryTabController.java
@@ -1,19 +1,29 @@
 package com.yaelev.sakilagui.controllers;
 
 
+import com.yaelev.sakilagui.dao.ActorDAO;
+import com.yaelev.sakilagui.dao.CountryDAO;
 import com.yaelev.sakilagui.entity.Country;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 import java.net.URL;
 import java.sql.Timestamp;
 import java.util.ResourceBundle;
 
 public class CountryTabController implements Initializable {
+
+    // Detta är en lookup tabell, ingen data i denna bör kunna ändras.
+    // Vi kan eventuellt lägga till en sökfunktion ifall vi har tid över.
+    
     @FXML
-    private TableView<Country> countryEntityTableViewEntityTableView;
+    private TableView<Country> countryTableView;
 
     @FXML
     private TableColumn<Country, Integer> countryIdColumn;
@@ -21,17 +31,19 @@ public class CountryTabController implements Initializable {
     @FXML
     private TableColumn<Country, String> countryNameColumn;
 
-
     @FXML
     private TableColumn<Country, Timestamp> countryLastUpdateColumn;
 
-    public void updateCountryEntityTableView() {
-        //Backended ÄR inte klart ännu
-
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        updateCountryTableView();
+    }
 
+    public void updateCountryTableView(){
+        countryTableView.setItems(FXCollections.observableList(new CountryDAO().read()));
+        countryIdColumn.setCellValueFactory(new PropertyValueFactory<>("countryId"));
+        countryNameColumn.setCellValueFactory(new PropertyValueFactory<>("country"));
+        countryLastUpdateColumn.setCellValueFactory(new PropertyValueFactory<>("lastUpdate"));
+        countryTableView.getItems().addAll();
     }
 }
