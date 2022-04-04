ca5b935faeb41d6585142448fa624442ac655f15
Daniel Bergeskar <daniel.bergeskar@yaelev.se>
2022-04-04 14:08:32 +0200

    jobb i stad

--- src/main/java/com/yaelev/sakilagui/controllers/CityTabController.java
+++ src/main/java/com/yaelev/sakilagui/controllers/CityTabController.java
@@ -1,37 +1,70 @@
 package com.yaelev.sakilagui.controllers;
 
+
+
 import com.yaelev.sakilagui.entity.City;
 import com.yaelev.sakilagui.entity.Country;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.sql.Timestamp;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 
-public class CityTabController implements Initializable {
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import com.yaelev.sakilagui.dao.CityDAO;
+import com.yaelev.sakilagui.dao.CountryDAO;
+
+
+
+public class CityTabController implements  Initializable{
     @FXML
-    private TableView<City> cityEntityTableView;
+    private TableView<City> cityTableView;
     @FXML
     private TableColumn<City, Integer> cityIdColumn;
     @FXML
     private TableColumn<City, String> cityNameColumn;
     @FXML
-    private TableColumn<City, Integer> countryIdColumn;
+    private TableColumn<Country, Integer> countryIdColumn;
     @FXML
     private TableColumn<City, Timestamp> cityLastUpdateColumn;
+    @FXML
+    private ChoiceBox<Country> countryChoiceBox;
 
-    public void updateAddressEntityTableView() {
-        //Backended ÄR inte klart ännu
 
+
+
+
+    //Backended ÄR inte klart ännu
+
+
+
+
+
+    public void updateCityTableView(){
+        cityTableView.setItems(FXCollections.observableList(new CityDAO().read()));
+        cityIdColumn.setCellValueFactory(new PropertyValueFactory<>("cityId"));
+        cityNameColumn.setCellValueFactory(new PropertyValueFactory<>("city"));
+        countryIdColumn.setCellValueFactory(new PropertyValueFactory<>("countryId"));
+        cityLastUpdateColumn.setCellValueFactory(new PropertyValueFactory<>("lastUpdate"));
+        cityTableView.getItems().addAll();
     }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        updateCityTableView();
+        updateCountryBox();
     }
+    public void updateCountryBox() {
+        countryChoiceBox.setItems(FXCollections.observableList(new CountryDAO().read()));
+        countryChoiceBox.getItems().addAll();
 
+    }
     public void updateCity_id() {
 
     }
